ccd122595f17551dd5dbe85eef1c4dd44bbef004
Alla Ponurko <ponurkoalla264@gmail.com>
2022-08-09 22:38:08 +0300

    Добавьте файлы проекта.

---
+++ WpfAppBookStore/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace WpfAppBookStore.Models
+{
+    public class Book
+    { 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public override string ToString()
+        {
+            return Title+" "+Author;
+        }
+
+    }
+}
